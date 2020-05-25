befebdae9656fc067a68955bd791a6f45d14217e
Pedro Cruz <pedrolucas11@live.com>
2020-05-25 20:48:22 -0300

    Configurado Controllers, e adicionado Layout do menu, está pendente a renomeação das views e configuração das mesmas

---
+++ ControleEstoque/ControleEstoque/Controllers/GraficoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControleEstoque.Controllers
+{
+    public class GraficoController : Controller
+    {
+        // GET: Grafico
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult PerdaMes()
+        {
+            return View();
+        }
+
+        public ActionResult EntradaSaidasMes()
+        {
+            return View();
+        }
+    }
+}
